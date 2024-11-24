577b8c05fa125a9c800bdbde2867ea68a2f52bb4
IzzyTrott <isabeltrottphotography@gmail.com>
2024-11-24 17:21:05 +0000

    added timezone to 'select' feature in html
    - Updated select element with cities and timezone values.
    - Add new function to js file (updateCity), changes info to city selected

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,6 @@
 function displayCities() {
   let londonElement = document.querySelector("#london");
+
   let londonDateElement = londonElement.querySelector(".date");
   let londonTimeElement = londonElement.querySelector(".time");
   let london = moment().tz("Europe/London");
@@ -14,6 +15,7 @@ function displayCities() {
   parisTimeElement.innerHTML = paris.format("HH:mm:ss");
 
   let newYorkElement = document.querySelector("#new-york");
+
   let newYorkDateElement = newYorkElement.querySelector(".date");
   let newYorkTimeElement = newYorkElement.querySelector(".time");
   let newYork = moment().tz("America/New_York");
@@ -28,6 +30,7 @@ function displayCities() {
   sydneyTimeElement.innerHTML = sydney.format("HH:mm:ss");
 
   let moscowElement = document.querySelector("#moscow");
+
   let moscowDateElement = moscowElement.querySelector(".date");
   let moscowTimeElement = moscowElement.querySelector(".time");
   let moscow = moment().tz("Europe/Moscow");
@@ -35,12 +38,29 @@ function displayCities() {
   moscowTimeElement.innerHTML = moscow.format("HH:mm:ss");
 
   let bangkokElement = document.querySelector("#bangkok");
+
   let bangkokDateElement = bangkokElement.querySelector(".date");
   let bangkokTimeElement = bangkokElement.querySelector(".time");
   let bangkok = moment().tz("Asia/Bangkok");
   bangkokDateElement.innerHTML = bangkok.format("ddd Do MMM YYYY");
   bangkokTimeElement.innerHTML = bangkok.format("HH:mm:ss");
 }
+
+function updateCity(event) {
+  let cityTimezone = event.target.value;
+  let cityName = cityTimezone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimezone);
+
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="update-city-grid">
+        <div><h2> ${cityName}</h2></div>
+        <div class="time">${cityTime.format("HH:mm:ss")}</div>
+        <div class="date">${cityTime.format("ddd Do MMM YYYY")}</div>
+      </div>`;
+}
+let citiesSelectElement = document.querySelector("#select-city");
+citiesSelectElement.addEventListener("change", updateCity);
 
 displayCities();
 setInterval(displayCities, 1000);
